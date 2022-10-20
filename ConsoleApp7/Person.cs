6da5eede9eb09ff8fb7ef2e0904621e7d1177daf
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-20 21:05:53 +0300

    Add project files.

---
+++ ConsoleApp7/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Person
+    {
+
+        //ליצור מחלקה חדשה של רכב 
+        //להגדיר 3 משתנים כסוג private 
+
+        // שם החברה של הרכב
+        // מספר הרכב
+        //שנת ייצור
+
+        // מגדירים pro של get ו set לכל אחד מהם 
+
+        //בדיקה  על ה prop של שנת ייצור
+        //בדיקה והשנת ייצור קטן מ 1991 
+        //לא לעשות השמה
+
+        //ליצור פונקציות
+        // פונקציה שמדפיסה את שם החברה של הרכב
+        //פונקציה שמדפיסה את המספר של הרכב
+        //פונקציה שלישית שמדפיסה את המספר של שנת ייצור
+
+
+
+        private string _firstName; //ctr + rr / f2/ rename 
+        private string _lastName;
+        private int _age;
+
+
+        //הכימוס (encapsulation).
+
+
+        //prop
+        public string FirstName
+        {
+            get { return _firstName; }//מחזיר את המידע
+            set { _firstName = value; }//השמה למשתנה
+        }
+
+        public string LastName
+        {
+            set { _lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+            }
+        }
+
+
+
+        public void Print()
+        {
+            Console.WriteLine($"{_firstName}{_lastName}{_age}");
+        }
+
+
+        //public int GetAge()
+        //{
+        //    return age;
+        //}
+
+        //public bool Check()
+        //{
+        //    return firstName.Length > 5;
+        //}
+
+
+
+
+
+        //פונקציה  שמחזירה את הגיל של המשתמש 
+
+        //פונקציה שמקבלת את השם של המשתמש   ובודקת האם המחרוזת האורך שלה יותר מ5 
+        //במידה וכן מחזירה true
+        //אחרת false
+
+
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp7;
+using System;
+
+namespace OOPLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps1 = new Person();
+            ps1.Age = 60;
+            ps1.FirstName = " sdfdsf";
+            ps1.LastName = " sdfdsf";
+
+
+            Person ps2 = new Person();
+            ps2.Age = 70;
+            ps2.FirstName = "yarin";
+            ps2.LastName = " shaer";
+            Console.WriteLine($"{ps1.Age}{ps2.Age}");
+
+            Person ps11 = new Person
+            {
+                 Age= 60,
+                 FirstName = "sdf",
+                 LastName  = " shaer",
+
+             
+            };
+            Console.WriteLine(ps11.FirstName);
+
+
+            //Console.WriteLine(ps1.Age);
+            //ps1.Age = 40;
+            //Console.WriteLine(ps1.Age);
+
+
+
+
+            //ps.firstName = "yarin";
+            //ps.lastName = "sh";
+            //ps.age = 50;
+
+            //Console.WriteLine(ps.GetAge());
+            //Console.WriteLine(ps.Check());
+            //להדפיס האם האורך של המחרוזת   יותר מ5 ע"י שימוש בפונקציה שבודקת
+
+        }
+    }
+}
